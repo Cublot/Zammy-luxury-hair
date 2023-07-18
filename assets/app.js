f718b45bcfef46d7eb3e8f37628fac2d3de12290
Cublot <125982179+Cublot@users.noreply.github.com>
2023-07-18 13:56:21 +0100

    okay

--- assets/app.js
+++ assets/app.js
@@ -13,6 +13,11 @@ let poster = document.getElementById('poster');
 let title = document.getElementById('title');
 let price = document.getElementById('price');
 
-Array.from(document.getElementsByClassName{'card'}).forEach({ele, i}=>
-    // console.log{ele}
+Array.from(document.getElementsByClassName('card')).forEach((ele, i)=> {
+    // console.log(ele)
+    ele.addEventListener('click', ()=> {
+        poster.src = ele.getElementsByTagName('img')[0].src;
+        title.innerText = ele.getElementsByTagName('h4')[0].innerText;
+        price-cont.innerText - ele.getElementsByTagName('h5')[0].innerText
+   })
 })
